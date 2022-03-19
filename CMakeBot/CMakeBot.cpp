653e50d6530e3ebb01c3acc62ff1e5dd8cdf4b31
Антон Побылица <apobylitsa@GMAIL.COM>
2022-03-19 14:05:09 +0300

    first turn fix

--- CMakeBot/CMakeBot.cpp
+++ CMakeBot/CMakeBot.cpp
@@ -69,94 +69,96 @@ int main()
         data = wg.send_data({ Action::GAME_STATE });
         GameState::get().update(json::parse(data.json_data));
         if (GameState::get().finished) break;
-        if (GameState::get().players.size() == pl.num_players && GameState::get().current_player_idx == bot.idx) {
+        if (GameState::get().players.size() == pl.num_players) {
+            if (GameState::get().current_player_idx == bot.idx) {
 
-            g.update();
+                g.update();
 
-            cout << "start my work" << endl;
-            data = wg.send_data({ Action::MAP });
-            Map::get().update(json::parse(data.json_data));
+                cout << "start my work" << endl;
+                data = wg.send_data({ Action::MAP });
+                Map::get().update(json::parse(data.json_data));
 
-            vector<int> can_be_attacked;
-            for (const auto& player : GameState::get().players) {
-                if (is_enemy_neutral(bot.idx, player.idx)) {
-                    can_be_attacked.push_back(player.idx);
+                vector<int> can_be_attacked;
+                for (const auto& player : GameState::get().players) {
+                    if (is_enemy_neutral(bot.idx, player.idx)) {
+                        can_be_attacked.push_back(player.idx);
+                    }
                 }
-            }
-            for (int i = 0; i < can_be_attacked.size(); ++i) {
-                cout << can_be_attacked[i] << " ";
-            }
-            cout << endl;
-
-            vector<MapCode> map_matrix((2 * rad + 1) * (2 * rad + 1), MapCode::NOTHING);
-
-            for (int i = 0; i < Map::get().obstacle.size(); ++i) {
-                map_matrix[code(Map::get().obstacle[i], rad)] = MapCode::OBSTACLE;
-            }
-            for (int i = 0; i < Map::get().catapult.size(); ++i) {
-                map_matrix[code(Map::get().catapult[i], rad)] = MapCode::CATAPULT;
-            }
-
-
-            set<Tank*, decltype(&Tank::cmp)> tanks_can_be_attacked(&Tank::cmp);
-            set<pair<int, Tank*>, decltype(&Tank::cmp2)> how_many_can_attack(&Tank::cmp2);
-
-            for (int i = 0; i < can_be_attacked.size(); ++i) {
-                for (int j = 0; j < GameState::get().vehicles[can_be_attacked[i]].size(); ++j) {
-                    tanks_can_be_attacked.insert(GameState::get().vehicles[can_be_attacked[i]][j].get());
+                for (int i = 0; i < can_be_attacked.size(); ++i) {
+                    cout << can_be_attacked[i] << " ";
                 }
-            }
-            cout << "Tanks can be attacked size : " << tanks_can_be_attacked.size() << endl;
-            for (auto& my_tank : GameState::get().vehicles[bot.idx]) {
-                int counter = 0;
+                cout << endl;
+
+                vector<MapCode> map_matrix((2 * rad + 1) * (2 * rad + 1), MapCode::NOTHING);
+
+                for (int i = 0; i < Map::get().obstacle.size(); ++i) {
+                    map_matrix[code(Map::get().obstacle[i], rad)] = MapCode::OBSTACLE;
+                }
+                for (int i = 0; i < Map::get().catapult.size(); ++i) {
+                    map_matrix[code(Map::get().catapult[i], rad)] = MapCode::CATAPULT;
+                }
+
+
+                set<Tank*, decltype(&Tank::cmp)> tanks_can_be_attacked(&Tank::cmp);
+                set<pair<int, Tank*>, decltype(&Tank::cmp2)> how_many_can_attack(&Tank::cmp2);
+
+                for (int i = 0; i < can_be_attacked.size(); ++i) {
+                    for (int j = 0; j < GameState::get().vehicles[can_be_attacked[i]].size(); ++j) {
+                        tanks_can_be_attacked.insert(GameState::get().vehicles[can_be_attacked[i]][j].get());
+                    }
+                }
+                cout << "Tanks can be attacked size : " << tanks_can_be_attacked.size() << endl;
+                for (auto& my_tank : GameState::get().vehicles[bot.idx]) {
+                    int counter = 0;
+                    for (auto& target : tanks_can_be_attacked) {
+                        if (my_tank->can_attack(*target, map_matrix)) ++counter;
+                    }
+                    how_many_can_attack.insert(make_pair(counter, my_tank.get()));
+                }
+
+                vector<Tank*> attack_to_destroy(5, nullptr);
+
                 for (auto& target : tanks_can_be_attacked) {
-                    if (my_tank->can_attack(*target, map_matrix)) ++counter;
-                }
-                how_many_can_attack.insert(make_pair(counter, my_tank.get()));
-            }
+                    int counter_health = target->health;
+                    for (int i = 0; i < GameState::get().vehicles[bot.idx].size(); ++i) {
+                        if (attack_to_destroy[i]) continue;
+                        if (GameState::get().vehicles[bot.idx][i]->can_attack(*target, map_matrix)) {
+                            --counter_health;
+                        }
+                    }
 
-            vector<Tank*> attack_to_destroy(5, nullptr);
+                    if (counter_health > 0) continue;
 
-            for (auto& target : tanks_can_be_attacked) {
-                int counter_health = target->health;
-                for (int i = 0; i < GameState::get().vehicles[bot.idx].size(); ++i) {
-                    if (attack_to_destroy[i]) continue;
-                    if (GameState::get().vehicles[bot.idx][i]->can_attack(*target, map_matrix)) {
-                        --counter_health;
+                    counter_health = target->health;
+                    for (auto& [points, my_tank] : how_many_can_attack) {
+                        if (attack_to_destroy[(int)my_tank->type]) continue;
+                        if (my_tank->can_attack(*target, map_matrix)) {
+                            attack_to_destroy[(int)my_tank->type] = target;
+                            --counter_health;
+                        }
+                        if (counter_health == 0) break;
                     }
                 }
 
-                if (counter_health > 0) continue;
+                for (int i = 0; i < attack_to_destroy.size(); ++i) {
+                    cout << attack_to_destroy[i] << " ";
+                }
+                cout << endl;
 
-                counter_health = target->health;
-                for (auto& [points, my_tank] : how_many_can_attack) {
-                    if (attack_to_destroy[(int)my_tank->type]) continue;
-                    if (my_tank->can_attack(*target, map_matrix)) {
-                        attack_to_destroy[(int)my_tank->type] = target;
-                        --counter_health;
+                for (const auto& [id, vehicles] : GameState::get().vehicles) {
+                    for (const auto& vehicle : vehicles) {
+                        map_matrix[code(vehicle->position, rad)] = (id == bot.idx) ? MapCode::FRIENDLY_TANK : MapCode::ENEMY_TANK;
                     }
-                    if (counter_health == 0) break;
                 }
-            }
 
-            for (int i = 0; i < attack_to_destroy.size(); ++i) {
-                cout << attack_to_destroy[i] << " ";
-            }
-            cout << endl;
-
-            for (const auto& [id, vehicles] : GameState::get().vehicles) {
-                for (const auto& vehicle : vehicles) {
-                    map_matrix[code(vehicle->position, rad)] = (id == bot.idx) ? MapCode::FRIENDLY_TANK : MapCode::ENEMY_TANK;
+                for (auto& curr_tank : GameState::get().vehicles[bot.idx]) {
+                    data = curr_tank->step(map_matrix, tanks_can_be_attacked, attack_to_destroy);
+                    if (data.code != -1) wg.send_data(data);
                 }
+                cout << "********************************************************************************************************************" << endl;
             }
-
-            for (auto& curr_tank : GameState::get().vehicles[bot.idx]) {
-                data = curr_tank->step(map_matrix, tanks_can_be_attacked, attack_to_destroy);
-                if (data.code != -1) wg.send_data(data);
-            }
-            cout << "********************************************************************************************************************" << endl;
+            wg.send_data({ Action::TURN });
         }
-        wg.send_data({ Action::TURN });
     }
     cout << "Winner : " << GameState::get().winner << endl;
     cout << data.code << " " << data.json_data << endl;
